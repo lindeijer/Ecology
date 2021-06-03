5d8ce476e87d37bdde152744ece359520d38eb0b
David Lindeijer <david@lindeijer.nl>
2021-06-03 23:54:49 +0200

    Expect to use Spark.

--- build.sbt
+++ build.sbt
@@ -2,7 +2,8 @@ name := "ecology"
 
 version := "0.1"
 
-scalaVersion := "2.13.5"
+scalaVersion := "2.12.10"
+// scalaVersion := "2.13.5"
 
 idePackagePrefix := Some("nl.dgl.ecology")
 
@@ -11,3 +12,6 @@ libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.7"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.7" % "test"
 
 // libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % Test  // FunSuite
+
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.2"
--- src/main/scala/v2/StockSystem.scala
+++ src/main/scala/v2/StockSystem.scala
@@ -11,14 +11,21 @@ class StockFlowSystem {
   var streams = List.empty[(TypeOfStock,FlowRate,TypeOfStock)]
 
   def stream(src:TypeOfStock,flowRate:FlowRate,dst:TypeOfStock): Unit = {
-    streams = streams.appended((src,flowRate,dst));
+    streams = streams :+ (src,flowRate,dst);
   }
 
   def stepFlow(dT: Double,stock:Stock): Stock = {
+
+    def addFlows (l:Double,r:Double):Double = l+r
+
     val node2flow = streams //
       .map { case (src,flowRate,dst) => (src, flowRate(dT,stock),dst) }
       .flatMap { case (src,flow,dst) => List((src,-flow),(dst,+flow)) }
-      .groupMapReduce ({ case (node, _) => node })( {case (_, flow) => flow})( {case (l,r) => l + r} );
+      // .groupMapReduce ({ case (node, _) => node })  ( {case (_, flow) => flow})  ( {case (l,r) => l + r} );
+      //    def groupMapReduce[K, B](key: (A) => K)(f: (A) => B)(reduce: (B, B) => B): Map[K, B]
+      //    It is equivalent to groupBy(key).mapValues(_.map(f).reduce(reduce)), but more efficient.
+      .groupBy({ case (node, _) => node })
+      .mapValues(  _.map( { case (_, flow) => flow } ).reduce( addFlows ))
 
     stock .map { case (node, level) => (node ,level + node2flow(node) )}
   }
